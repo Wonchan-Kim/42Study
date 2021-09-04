64a575b204cb952956d4cdb514655d18856d2810
Wonchan <wonchankim000827@gmail.com>
2021-09-04 11:41:14 +0900

    version 2

--- sudoku.c
+++ sudoku.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int sudoku[9][9] = {false, };
+int sudoku[9][9] = {0, };
 int check_row[9][10] = {false, };
 int check_column[9][10] = {false, };
 int check_square[9][10] = {false, };
-void print_ans(void); //print the result
+void get_sudoku(void); //print the result
 void solve_sudoku(void);//recursive function for solving sudoku
-int validity_check(void); //before checking the sudoku, check whether it is valid sudoku on the current status
+
 void get_sudoku(void)
 {
-	int user_input = 0;
 
 	   for(int i = 0; i<9; i++)
+	   {
+
 			for(int j = 0; j<9; j++)
 				{
-					scanf("%d", &user_input);
-					sudoku[i][j] = user_input;
+					printf("%d ", sudoku[i][j]);
+						
 				}
+			printf("\n");
+	   }
+
 }
 
 void solve_sudoku(void)
@@ -26,44 +30,52 @@ void solve_sudoku(void)
 	int r = -1;
 	int c = -1;
 	
-	for(int i = 0; i<9; i++)
+	for(int i = 1; i<9; i++)
+	{
 		for(int j = 0; j<9; j++)
-			if(sudoku[i][j] == false)
+		{
+			if(sudoku[i][j] == 0)
 			{
 				r = i;
 				c = j;
+				break;
 			}
+		}
+		if (r != -1) 
+			break;
+	}
 	
 	if(r == -1)
-		return;
+		get_sudoku();
+
 
 	int square = (r/3)*3 + c/3;
 	for(int number = 1; number<10; number++)
 		if(!check_row[r][number] && !check_column[c][number] && !check_square[square][number])
 			{
-				sudoku[r][c] = number;
 				check_row[r][number] = check_column[c][number] = check_square[square][number] = true;
+				sudoku[r][c] = number;
 				solve_sudoku();
 			}
 }
 
 int main(void)
 {
-	int save_r, save_c;
+	int save_r, save_c, square;
 	int user_input = 0;
 	for(int i = 0; i< 9; i++)
 		for(int j= 0; j<9; j++)
 			{
 				 scanf("%d ", &user_input);
-				 sudoku[i][i] = user_input;
+				 sudoku[i][j] = user_input;
 				 save_r = i;
 				 save_c = j;
+			    square = (save_r/3)*3 + save_c/3;
+				check_row[save_r][user_input] = true;
+				check_column[save_c][user_input] = true;
+				check_square[square][user_input] = true;
 	    	}
 	
-	int square = (save_r/3)*3 + save_c/3;
-	check_row[save_r][user_input] = true;
-	check_column[save_c][user_input] = true;
-	check_square[square][user_input] = true;
 
 	printf("----------------------------");
 	solve_sudoku();
